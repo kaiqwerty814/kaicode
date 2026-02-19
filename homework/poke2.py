e1bf53b15c91c28ceb992366d1137824b183cd0a
kaiqwerty814 <khayir@gmail.com>
2026-02-19 17:12:03 -0500

    done

--- homework/poke2.py
+++ homework/poke2.py
@@ -1,1 +1,13 @@
-#name, id and height
+import requests
+
+data = "https://pokeapi.co/api/v2/pokemon/squirtle"
+
+response = requests.get(data)
+
+mutant_data = response.json
+
+print(mutant_data["name"])
+
+print(mutant_data["base_expirience"])
+
+print(mutant_data[""])
