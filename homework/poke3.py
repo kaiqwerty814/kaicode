4acdb897b17a0c08a3d3402d50d68f1ac9a8fe23
kaiqwerty814 <khayir@gmail.com>
2026-03-12 17:19:28 -0400

    fin

--- homework/poke3.py
+++ homework/poke3.py
@@ -1,1 +1,13 @@
-#name, id and height
+import requests
+
+rna = "https://pokeapi.co/api/v2/pokemon/gengar"
+
+response = requests.get(rna)
+
+dna = response.json()
+
+print(dna["name"])
+
+print(dna["base_experience"])
+
+print(dna["game_indices"])
